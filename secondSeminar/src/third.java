25431b145a6abdb782a0e1987d558c7e334c987c
misha1potapenko <misha1potapenko@yandex.ru>
2022-11-29 22:04:44 +0300

    added finish solution

--- secondSeminar/src/third.java
+++ secondSeminar/src/third.java
@@ -1,14 +1,15 @@
 //В файле содержится строка с данными:
 //        [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
-//        {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+//        {"фамилия":"Петров","оценка":"4","предмет":"Информатика"},
 //        {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
 //        Напишите метод, который разберёт её на составные части и,
 //        используя StringBuilder создаст массив строк такого вида:
 //        Студент Иванов получил 5 по предмету Математика.
-//        Студент Петрова получил 4 по предмету Информатика.
+//        Студент Петров получил 4 по предмету Информатика.
 //        Студент Краснов получил 5 по предмету Физика.
 
 
+import javax.management.StringValueExp;
 import java.io.*;
 
 public class third {
@@ -17,16 +18,14 @@ public class third {
 
         try(FileReader fr = new FileReader("txtForThird.txt"))
         {
-            // читаем посимвольно
+
             BufferedReader reader = new BufferedReader(fr);
             String str1;
             while((str1 = reader.readLine())!= null){
-//                System.out.println(str);
+
                 String str2 = str1.replace("\"","");
                 String str = str2.replace("}","");
                 String[] words = str.split(",");
-//                for (String name1: words) {
-//                    System.out.println(name1);
                   String forName = words[0];
                   String forCountry = words[1];
                   String forCity = words[2];
@@ -43,7 +42,15 @@ public class third {
                   sql.append("по предмету ");
                   sql.append(subject[1]);
                   sql.append(" ");
-                  System.out.println(sql);
+//                  System.out.println(sql);
+                String[] array = new String[]{sql.toString()};
+
+
+                for (String agf: array) {
+                    System.out.println(agf);
+                }
+
+
 
             }
         }
