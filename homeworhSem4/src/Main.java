e54be154d9c6ec1d898932a4cd162058b790eb52
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-02 12:28:45 +0300

    added new file for homework 4

---
+++ homeworhSem4/src/Main.java
@@ -0,0 +1,10 @@
+//Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
