ad2efc718645ced85c1ee8061475d1bc3ee0149e
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-02 18:16:50 +0300

    added list reversr

--- homeworhSem4/src/Main.java
+++ homeworhSem4/src/Main.java
@@ -1,6 +1,7 @@
 //Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -9,17 +10,30 @@ public class Main {
         LinkedList<Integer> listForReturn = new LinkedList<>();
         System.out.println("Введите количество элементов для добавления в LinkedList: ");
         Scanner forLinked = new Scanner(System.in);
+
         int quantity = forLinked.nextInt();
         for (int i = 0; i < quantity ; i++) {
             listForReturn.add(i,i);
-
         }
         System.out.print(listForReturn + " ");
 
-                System.out.println();
+        ListReverse(listForReturn);
+        System.out.print(listForReturn + " ");
     }
-    public static void returnLinkedList(LinkedList<Integer>){
-        System.out.println();
 
+    public static LinkedList<Integer> ListReverse(LinkedList<Integer> listForReturn) {
+        int count = 0;
+        for (int i = listForReturn.size()-1; i >= listForReturn.size()/2; i--) {
+            int temp = listForReturn.get(count);
+            int temp2 = listForReturn.get(i);
+            listForReturn.set(i,temp);
+            listForReturn.set(count,temp2);
+            count++;
+        }
+        return listForReturn;
     }
+
 }
+
+
+
