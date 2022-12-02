3779e8fb424e2b6182da89cb4211382a2fe1d9b2
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-02 10:30:26 +0300

    added hello

--- fourthSem/src/Main.java
+++ fourthSem/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        printAnyThink();
+    }
+    public static void printAnyThink(){
+        System.out.println("How are you?");
     }
 }
