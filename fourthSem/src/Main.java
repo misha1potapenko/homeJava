5147b19dbcc2edb580d2545888b1c6b0d52f4185
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-02 11:42:37 +0300

    added two function

--- fourthSem/src/Main.java
+++ fourthSem/src/Main.java
@@ -2,9 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        printAnyThink();
+
+        float res1 = results(5f,7.3f);
+        printAnyThink(String.valueOf(res1));
+
     }
-    public static void printAnyThink(){
-        System.out.println("How are you?");
+    public static void printAnyThink(String any){
+        System.out.println(any);
+    }
+
+    public static float results(float a, float b) {
+        float res = a + b;
+        return res;
+
     }
 }
