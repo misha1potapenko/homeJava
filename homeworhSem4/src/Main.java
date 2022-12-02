2fe85d315a927e82072bc4d1b31a8145dfbd127a
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-02 12:40:02 +0300

    added for i

--- homeworhSem4/src/Main.java
+++ homeworhSem4/src/Main.java
@@ -1,10 +1,24 @@
 //Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
 
 
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        LinkedList<Integer> listForReturn = new LinkedList<>();
+        System.out.println("Введите количество элементов для добавления в LinkedList: ");
+        Scanner forLinked = new Scanner(System.in);
+        int quantity = forLinked.nextInt();
+        for (int i = 0; i < quantity ; i++) {
+            listForReturn.add(i,i);
 
-        System.out.println("Hello world!");
+        }
+        System.out.print(listForReturn + " ");
+        System.out.println();
+        for (int i = quantity; i > 0 ; i--) {
+            listForReturn.add(i, i);
+        }
+        System.out.print(listForReturn + " ");
     }
 }
