9dced90c52b92c1a5ebed8bd2b0ae2ec3f034661
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-03 20:02:23 +0300

    added second task solution

--- homeworhSem4/src/second.java
+++ homeworhSem4/src/second.java
@@ -1,5 +1,38 @@
-public class second {
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+//        enqueue() - помещает элемент в конец очереди, dequeue() - возвращает первый
+//        элемент из очереди и удаляет его, first() - возвращает первый элемент из очереди, не удаляя.
+
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class for4Sem {
     public static void main(String[] args) {
-        System.out.println("sdsdsdsd");
+        LinkedList<Integer> firstLastPosition = new LinkedList<>();
+        firstLastPosition.add(2332);
+        firstLastPosition.add(2);
+        firstLastPosition.add(456);
+        System.out.println(firstLastPosition);
+//        enqueue(firstLastPosition);
+//        first(firstLastPosition);
+        dequeue(firstLastPosition);
+        System.out.println(firstLastPosition);
+    }
+    public static LinkedList<Integer> enqueue(LinkedList<Integer> linkedList){
+        System.out.print("Введите число для добавления в конец LinkedList: ");
+        Scanner forFinish = new Scanner(System.in);
+        int finish = forFinish.nextInt();
+        linkedList.addLast(finish);
+        return linkedList;
+    }
+    public static void  first(LinkedList<Integer> linkedList) {
+
+        int first = linkedList.getFirst();
+        System.out.println(first);
+    }
+    public static LinkedList<Integer> dequeue(LinkedList<Integer> linkedList){
+        int first = linkedList.pollFirst();
+        System.out.println(first);
+        return linkedList;
     }
 }
