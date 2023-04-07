383b54b42ca7aa3f6a42d3a677f25c58cfeab32d
misha1potapenko <misha1potapenko@yandex.ru>
2023-04-07 20:26:35 +0300

    added function add node

--- fourthSem/src/Tree/LessonReadBlackTree.java
+++ fourthSem/src/Tree/LessonReadBlackTree.java
@@ -1,2 +1,47 @@
-package Tree;public class LessonReadBlackTree {
+package Tree;
+
+import java.awt.*;
+
+public class LessonReadBlackTree {
+    private Node root;
+
+    public boolean add(int value) {}
+
+    private boolean addNode(Node node, int value){
+        if (node.value == value){
+            return false;
+        }
+    }
+
+    private Node rebalance(Node node) {}
+
+    private Node rightSwap(Node node){}
+
+    private Node leftSwap(Node node){}
+
+
+
+
+
+
+
+    private class Node {
+        private int value;
+        private Color color;
+        private Node leftChild;
+        private Node rightChild;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", color=" + color +
+                    ", leftChild=" + leftChild +
+                    ", rightChild=" + rightChild +
+                    '}';
+        }
+    }
+    private enum Color{
+        RED, BLACK
+    }
 }
