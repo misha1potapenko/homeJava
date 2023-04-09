e6cc11ee5a89f4406e4e026a3943de9e5f345b88
misha1potapenko <misha1potapenko@yandex.ru>
2023-04-09 11:11:32 +0300

    added full solution for finish project

--- ReadBlackTree/Three/src/Main.java
+++ ReadBlackTree/Three/src/Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final LessonReadBlackTree tree = new LessonReadBlackTree();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            while (true){
+                try{
+                    int value = Integer.parseInt((reader.readLine()));
+                    tree.add(value);
+                    System.out.println("finish");
+                }catch (Exception ignored){
+
+                }
+            }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 }
