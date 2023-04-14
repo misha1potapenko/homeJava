09ca54b180af7b714cee3a3bed61bda07b7a36d1
misha1potapenko <misha1potapenko@yandex.ru>
2023-04-14 20:01:54 +0300

    added solution for task1

--- Exeption/lesson2/src/InputFloat.java
+++ Exeption/lesson2/src/InputFloat.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
@@ -13,4 +15,35 @@
 */
 
 public class InputFloat {
-}
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        inputUser();
+
+    }
+
+    /**
+     Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+     и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+     вместо этого, необходимо повторно запросить у пользователя ввод данных.
+     */
+    static float inputUser(){
+
+            while (true){
+                System.out.print("Введите число с плавающей точкой: ");
+                if (scanner.hasNextFloat()){
+                    float floatNumber = scanner.nextFloat();
+                    return floatNumber;
+                }
+                else{
+                    System.out.println("Число для поиска указано некорректно.\nПовторите попытку ввода.");
+                    scanner.nextLine();
+                }
+            }
+
+        }
+    }
+
+
+
