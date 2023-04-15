9ced516961c73466d2d6c79ecbcc0d26f3128dd7
misha1potapenko <misha1potapenko@yandex.ru>
2023-04-15 21:44:04 +0300

    added solution for task3

--- Exeption/lesson2/src/task3.java
+++ Exeption/lesson2/src/task3.java
@@ -7,21 +7,23 @@ import java.io.FileNotFoundException;
 public class task3 {
     public static void main(String[] args) throws Exception {
         try {
+            printSum(5,8);
             int a = 90;
             int b = 3;
+            int c = 0;
             System.out.println(a / b);
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        } catch (NullPointerException ex) {
+        } catch (ArithmeticException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
         }
     }
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+    public static void printSum(int a, int b) {
         System.out.println(a + b);
     }
 
