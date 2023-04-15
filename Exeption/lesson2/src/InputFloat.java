db62b5113960bc5907925ef29b768aedc197b20d
misha1potapenko <misha1potapenko@yandex.ru>
2023-04-15 18:28:49 +0300

    added task2

--- Exeption/lesson2/src/InputFloat.java
+++ Exeption/lesson2/src/InputFloat.java
@@ -20,6 +20,7 @@ public class InputFloat {
 
     public static void main(String[] args) {
         inputUser();
+        test2();
 
     }
 
@@ -40,6 +41,18 @@ public class InputFloat {
                     System.out.println("Число для поиска указано некорректно.\nПовторите попытку ввода.");
                     scanner.nextLine();
                 }
+            }
+
+        }
+        //2. Если необходимо, исправьте данный код
+        //  (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+        static void test2(){
+            try {
+                int d = 0;
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } catch (ArithmeticException e) {
+                System.out.println("Catching exception: " + e);
             }
 
         }
