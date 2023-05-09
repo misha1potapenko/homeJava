7c3c19290a1e9c6d6d503768874c182476b9207f
misha1potapenko <misha1potapenko@yandex.ru>
2023-05-09 18:03:18 +0300

    added while true

--- Exeption/finishProject/src/Input.java
+++ Exeption/finishProject/src/Input.java
@@ -16,45 +16,58 @@ public class Input {
         String name = scanner.next();
         System.out.println("Введите отчетво: ");
         String patronymic = scanner.next();
+
         System.out.println("Введите дату рождения формата dd.mm.yyyy: ");
         String birthday = scanner.next();
         // проверяем что ввел пользователь (переберем строку по элементам)
         String forBirthday = "12.12.1988"; // для проверки создаем образец
-        String phone = null;
+
+        while (true) {
+            try {
+                if (birthday.charAt(2) == forBirthday.charAt(2) && birthday.charAt(5) == forBirthday.charAt(5) &&
+                        birthday.length() == 10)  {
+                    break;}
+                else {
+                    System.out.println("Введите корректно дату рождения ");
+                    birthday = scanner.next();
+                    break;
+                }
+            } catch (StringIndexOutOfBoundsException ex2){
+
+                System.out.println(ex2.getMessage());
+                System.out.println("Введите корректно дату рождения ");
+                birthday = scanner.next();
+                break;
+            }
 
 
-        try {
-            System.out.println("Введите номер телефона ");
-            phone = Integer.toString(scanner.nextInt()); // преобразовали int  в строку, так как в файл пишется строка
-        } catch (Exception ex1){
-            System.out.println(ex1.getMessage());
+
         }
 
+
+
+
+        String phone = null;
+        while (true) {
+            try {
+                System.out.println("Введите номер телефона ");
+                phone = Integer.toString(scanner.nextInt()); // преобразовали int  в строку, так как в файл пишется строка
+                break;
+            } catch (Exception ex1) {
+                System.out.println(ex1.getMessage());
+            }
+        }
         String gender = null;
         while (true){
             System.out.println("Введите пол (f или m ): ");
             gender = scanner.next();
-            if (gender.equals("f")) break;
-            else System.out.println("Введите f или m");
-            if (gender.equals("m")) break;
-            else System.out.println("Введите f или m");
-        }
-
-
-        try {
-            for (int i = 0; i < birthday.length(); i++) {
-                if (birthday.charAt(2) != forBirthday.charAt(2) || birthday.charAt(5) != forBirthday.charAt(5)){
-                    System.out.println("Вы ввели не корректно точки  \n" +
-                            "Введите дату рождения формата dd.mm.yyyy: ");
-                    birthday = scanner.next();
-
-                }
-
+            if (gender.equals("f") || gender.equals("m")) {
+                break;
             }
-        }    catch(Exception ex) {
-
-            System.out.println(ex.getMessage());
+                else System.out.println("Вы ввели не верно, введите один из вариантов");
         }
+
+
 
 
 
